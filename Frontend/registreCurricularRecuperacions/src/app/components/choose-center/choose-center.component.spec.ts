08c596a5f794c3faf274b8080b62164022bf68ac
ALZAMORA1\storrent <storrent@alzamoragroup.com>
2025-05-30 10:26:03 +0200

    TEST ngOnInit(): should set errorMessage when no temp data

---
+++ Frontend/registreCurricularRecuperacions/src/app/components/choose-center/choose-center.component.spec.ts
@@ -0,0 +1,30 @@
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { ChooseCenterComponent } from './choose-center.component';
+
+describe('ChooseCenterComponent', () => {
+  let fixture: ComponentFixture<ChooseCenterComponent>;
+  let comp: ChooseCenterComponent;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ChooseCenterComponent]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ChooseCenterComponent);
+    comp = fixture.componentInstance;
+  });
+
+  it('should create the component instance', () => {
+    expect(comp).toBeTruthy();
+  });
+
+  it('ngOnInit(): should set errorMessage when no temp data', () => {
+    spyOn(localStorage, 'getItem').and.returnValue(null);
+
+    comp.ngOnInit();
+
+    expect(comp.errorMessage).toBe('No center data.');
+    expect(comp.userUUID).toBe('');
+    expect(comp.centers).toEqual([]);
+  });
+});
