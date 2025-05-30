5500142439d949c54d9095f9643dbb700130e99e
ALZAMORA1\storrent <storrent@alzamoragroup.com>
2025-05-30 09:30:10 +0200

    TEST loadCenters should populate centers with data from the service

---
+++ Frontend/registreCurricularRecuperacions/src/app/components/change-center/change-center.component.spec.ts
@@ -0,0 +1,37 @@
+import { of } from 'rxjs';
+import { ChangeCenterComponent } from './change-center.component';
+import { AuthService } from '../../../services/auth.service';
+import { Router } from '@angular/router';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+
+
+describe('ChangeCenterComponent', () => {
+  let comp: ChangeCenterComponent;
+  let fixture;
+  const mockCenters = [{ centerName: 'Centro1', role: 1 }];
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ChangeCenterComponent],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: { listMyCenters: () => of(mockCenters) }
+        },
+        { provide: Router, useValue: {} }
+      ]
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(ChangeCenterComponent);
+    comp = fixture.componentInstance;
+  });
+
+  it('should create the component instance', () => {
+    expect(comp).toBeTruthy();
+  });
+
+  it('loadCenters() should populate centers with data from the service', () => {
+    comp.loadCenters();
+    expect(comp.centers).toEqual(mockCenters);
+  });
+});
---
+++ Frontend/registreCurricularRecuperacions/src/app/components/change-center/change-center.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-change-center',
+  imports: [],
+  templateUrl: './change-center.component.html',
+  styleUrl: './change-center.component.css'
+})
+export class ChangeCenterComponent {
+
+}
