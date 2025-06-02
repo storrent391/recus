141d65e45dcbcbd5e81f430d4b8464bc5e98e998
ALZAMORA1\storrent <storrent@alzamoragroup.com>
2025-06-02 15:02:41 +0200

    Main update

--- Frontend/registreCurricularRecuperacions/src/main.ts
+++ Frontend/registreCurricularRecuperacions/src/main.ts
@@ -1,6 +1,13 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { routes } from './app/app.routes';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { authInterceptor } from './interceptors/auth.interceptor';
 
-bootstrapApplication(AppComponent, appConfig)
-  .catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([authInterceptor])),
+  ],
+}).catch((err) => console.error(err));
