f08c6c969738eec7ec8a788b40a9b8f6c83d092e
ALZAMORA1\storrent <storrent@alzamoragroup.com>
2025-06-02 09:29:57 +0200

    Updated page not found

--- Frontend/registreCurricularRecuperacions/src/app/components/page-not-found/page-not-found.component.ts
+++ Frontend/registreCurricularRecuperacions/src/app/components/page-not-found/page-not-found.component.ts
@@ -1,11 +1,20 @@
+
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-page-not-found',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule, RouterModule],
   templateUrl: './page-not-found.component.html',
-  styleUrl: './page-not-found.component.css'
+  styleUrls: ['./page-not-found.component.css']
 })
 export class PageNotFoundComponent {
+  constructor(private router: Router) {}
 
+  goDashboard(): void {
+    this.router.navigate(['/dashboard']);
+  }
 }
