f240f6d7fa92212f3a8b9918842b236a45ceb1a7
ALZAMORA1\storrent <storrent@alzamoragroup.com>
2025-06-02 10:14:52 +0200

    IMPLEMENTED admin guard

---
+++ Frontend/registreCurricularRecuperacions/src/guards/admin-center.guard.ts
@@ -0,0 +1,25 @@
+// src/app/guards/admin-center.guard.ts
+import { CanActivateFn, Router, UrlTree } from '@angular/router';
+import { inject } from '@angular/core';
+
+export const AdminCenterGuard: CanActivateFn = (
+  route,
+  state
+): boolean | UrlTree => {
+  const router = inject(Router);
+  const token = localStorage.getItem('token');
+  if (!token) {
+    return router.parseUrl('/login');
+  }
+
+  try {
+    const payload = JSON.parse(atob(token.split('.')[1]));
+    if (payload.centerRole === 2) {
+      return true;
+    }
+  } catch (e) {
+    console.warn('JWT no vàlid', e);
+  }
+
+  return router.parseUrl('/unauthorized');
+};
