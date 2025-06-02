c8a3a9f6941dd162728d1bd9f0aed3b35f8b7c36
ALZAMORA1\storrent <storrent@alzamoragroup.com>
2025-06-02 11:30:56 +0200

    CSS UPDATE

---
+++ Frontend/registreCurricularRecuperacions/src/environments/environment.ts
@@ -0,0 +1,5 @@
+export const environment = {
+  production: false,
+  baseUrl: 'http://localhost:5000/apirc/v1',
+  GOOGLE_CLIENT_ID: '169472757700-ges24db9ar14vnk9duovj9m71eiblrdd.apps.googleusercontent.com'
+};
