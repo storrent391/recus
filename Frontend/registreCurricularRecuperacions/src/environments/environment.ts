d8d97eb4b6e58f3ba8fde28c8509d5781159362e
ALZAMORA1\storrent <storrent@alzamoragroup.com>
2025-06-03 14:20:05 +0200

    Corrected login with google error

--- Frontend/registreCurricularRecuperacions/src/environments/environment.ts
+++ Frontend/registreCurricularRecuperacions/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
   baseUrl: 'http://localhost:5000/apirc/v1',
-  GOOGLE_CLIENT_ID: '169472757700-ges24db9ar14vnk9duovj9m71eiblrdd.apps.googleusercontent.com'
+  GOOGLE_CLIENT_ID: '15602931799-di7pfpqp9pu7g1fjhssesmprn5s7d3c0.apps.googleusercontent.com'
 };
